11278dc7f204d939087cfb516c266b141c5a4462
mfern <m.fern93@gmail.com>
2017-10-06 16:32:48 -0700

    major commit

--- js/para.js
+++ js/para.js
@@ -1,54 +1,24 @@
-
 $(document).ready(function() {
-var deleteLog = false;
-var placeHolder = $('#callbacks-placeholder');
-var imgFix = $("#img-fix-center");
-	
+  var deleteLog = true;
 
-$('#fullpage').fullpage({
-  anchors: ['page1', 'page2', 'page3', 'page4'],
-  sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
-  anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
-  responsiveWidth: 900,
-  onLeave: function(index, nextIndex, direction) {
-    if (deleteLog) {
-      placeHolder.html('');
-    }
-    if(nextIndex == 2){
-      imgFix.hide();
-    }
-    placeHolder.append('<p>onLeave - index:' + index + ' nextIndex:' + nextIndex + ' direction:' + direction + '</p>')
-  },
-  onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex) {
-    if (deleteLog) {
-      placeHolder.html('');
-    }
-    placeHolder.append('<p>onSlideLeave - anchorLink:' + anchorLink + " index:" + index + " slideIndex:" + slideIndex + " direction:" + direction + " nextSlideIndex:" + nextSlideIndex + '</p>');
-  },
-  afterRender: function() {
-    placeHolder.append('<p>afterRender</p>');;
-  },
-  afterResize: function() {
-    placeHolder.append('<p>afterResize</p>');
-  },
-  afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
-    placeHolder.append('<p>afterSlideLoad - anchorLink:' + anchorLink + " index:" + index + " slideAnchor:" + slideAnchor + " slideIndex:" + slideIndex + '</p>');
-    deleteLog = true;
-  },
-  afterLoad: function(anchorLink, index) {
-    placeHolder.append('<p>afterLoad - anchorLink:' + anchorLink + " index:" + index + '</p>');
-    deleteLog = true;
-    if(index == 3){
-      console.log("logged");
-      imgFix.css({"position": "fixed", "display":"block"}).fadeIn();
-    	//animate left
-    	
-    }
-  },
-  afterResponsive: function(isResponsive){
-    placeHolder.append('<p>afterResponsive - isResponsive:' +isResponsive + '</p>');
-  }
-});
+
+  $('#fullpage').fullpage({
+    sectionsColor: ['#f6faff', '#FFFFFF', '#F8F8FF', 'fffefc','#f6faff', 'smokeSceen', '#F8F8FF', '#F5F5F5',],
+    //anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', '5thPage', '6thpage', 'lastPage'],
+    //responsiveWidth: 900,
+    autoScrolling: false,
+    recordHistory: true,
+
+    lazyLoading: false,
+    
+    scrollOverflow: false,
+    scrollOverflowOptions: {
+          click: false
+     },
+  });
+
+
+
 
 });
 
@@ -56,18 +26,18 @@ $('#fullpage').fullpage({
 
 /*
 $('#fullpage').fullpage({
-	onSlideLeave: function( anchorLink, index, slideIndex, direction, nextSlideIndex){
-		var leavingSlide = $(this);
+  onSlideLeave: function( anchorLink, index, slideIndex, direction, nextSlideIndex){
+    var leavingSlide = $(this);
 
-		//leaving the first slide of the 2nd Section to the right
-		if(index == 2 && slideIndex == 0 && direction == 'right'){
-			alert("Leaving the fist slide!!");
-		}
+    //leaving the first slide of the 2nd Section to the right
+    if(index == 2 && slideIndex == 0 && direction == 'right'){
+      alert("Leaving the fist slide!!");
+    }
 
-		//leaving the 3rd slide of the 2nd Section to the left
-		if(index == 2 && slideIndex == 2 && direction == 'left'){
-			alert("Going to slide 2! ");
-		}
-	}
+    //leaving the 3rd slide of the 2nd Section to the left
+    if(index == 2 && slideIndex == 2 && direction == 'left'){
+      alert("Going to slide 2! ");
+    }
+  }
 });
 */
