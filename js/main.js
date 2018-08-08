6ddc3a4cc1f7a786b4f44357fcea83bc9549b9e8
mfern <m.fern93@gmail.com>
2018-08-08 14:05:31 -0700

    t commit -a

--- js/main.js
+++ js/main.js
@@ -1,115 +1,64 @@
-// /*$(document).ready(function() {
-//   var deleteLog = true;
+$( document ).ready(function() {
 
-//   document.onreadystatechange = function () {
-//   var state = document.readyState
-//     if( false){
-//       if (state == 'interactive') {
-//            document.getElementById('contents').style.visibility="hidden";
-//       } else if (state == 'complete') {
-//           setTimeout(function(){
-//              document.getElementById('interactive');
-//              document.getElementById('load').style.visibility="hidden";
-//              document.getElementById('fullpage').style.visibility="visible";
-//           },1000);
-//       }
-//     }
-//   }
-  
+	/*
+	console.log("passed");
+	$.ajax({url: "desktop.svg", success: function(result){
+        console.log(result);
+		$('#svg-wrapper').innerHTML(result)
+    }});
+    	console.log("passed2");
+    */
 
-//   $('#fullpage_home').fullpage({
-//     sectionsColor: ['#FFFFFF', '#f6faff', '#fcfcfc', 'fffefc','#FFFFFF', '#f6faff', 'smokeSceen', '#F8F8FF', '#F5F5F5',],
-//     anchors: ['1', '2', '3', '4', '5', '6', '7', '8thpage', '9thpage', '10thpage', '11thpage', '12thpage', '13thpage', '14thpage', '15thpage', '16thpage', '17thpage', '18thpage', '19thpage', '20thpage'],
-//     //responsiveWidth: 900,
-//     autoScrolling: false,
-//     recordHistory: true,
-//     lazyLoading: true,
-//     fitToSectionDelay: 1000000,
-    
-//     scrollOverflow: false,
-//     scrollOverflowOptions: {
-//           click: false
-//      },
-//   });
+	var d = new Date();
 
-// });
+	var months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+	var month = months[d.getMonth() ];
+	var date = d.getDate();
 
-// */
+	var url = window.location.href;
 
-// // Code borrowed from 
-// // https://jsfiddle.net/mariusc23/s6mLJ/31/
+	isLink = url.split("#");
 
-// (function($, window, document) {
-//   // Hide Header on on scroll down
-//     var didScroll;
-//     var lastScrollTop = 0;
-//     var delta = 4;
-//     var navbarHeight = $('header').outerHeight();
-//     var PR = window.devicePixelRatio || 1;
+	if(isLink.length > 1){
+		$("#portfolio-start").show(0);
+	} else{
+		$("#desktop").slideDown(1000, function(){
+			$("#portfolio-start").show(0, function(){
+				console.log("")
+			});
+		});
+	}
 
-//     $(window).scroll(function(event){
-//         didScroll = true;
-//     });
+	$("#month").html(month);
 
-//     setInterval(function() {
-//         if (didScroll) {
-//             hasScrolled();
-//             didScroll = false;
-//         }
-//     }, 250);
-    
-//     window.addEventListener('load', function() {
-        
-//         $(".portfolio-tile").each(function(){
-//             var inV = new inView(this);
-//             inV.onInView((function() {
-//                 $(this).addClass("animated slideInUp");
-//             }).bind(this));
-//         })
-        
-//          /*$( "#welcome-city-current" ).slideToggle( "slow", function() {
-            
-//           //$("#welcome-city").slideToggle("slow", function(){}) 
+	$("#date").html(date).attr("text-anchor", "middle");
 
-//           }); */
+	  // ===== Scroll to Top ==== 
+	$(window).scroll(function() {
+	    if ($(this).scrollTop() >= 50) {   
+	        $('#return-to-top').fadeIn(500);   
+	    } else {
+	        $('#return-to-top').fadeOut(350);  
+	    }
+	});
+	$('#return-to-top').click(function() {  
+	    $('body,html').animate({
+	        scrollTop : 0                 
+	    }, 150);
+	});
 
-//           $("#welcome-city-current").fadeTo(600, 0, "linear", function() {
-//               $(this).css("display", "none");
-//                 $("#welcome-city").fadeTo(1000, 1, "linear");
-            
-//           })
-          
-//     }); 
+	//window.sr = ScrollReveal();
+	//sr.reveal('.animate');
+
+	$(".link").on("click", function(){
+		$("#wholepage").fadeOut('slow', function() {
+			document.location = "default.html"
+		});
+	});
+});
 
 
-//     function hasScrolled() {
-//         var st = $(this).scrollTop();
-//         // Make sure they scroll more than delta
-//         if(Math.abs(lastScrollTop - st) <= delta)
-//             //return;
-
-//         if(navbarHeight == undefined)
-//             navbarHeight = $('header').outerHeight();
-
-//         // If they scrolled down and are past the navbar, add class .nav-up.
-//         // This is necessary so you never see what is "behind" the navbar.
-//         if (st > lastScrollTop){ // Scroll Down
-//             $('header').css("border-shadow", "none")
-//             $('header').removeClass('header-shadow-off slideInDown').addClass("slideOutUp");
-//         } else { // Scroll Up
-//             if(st + $(window).height() < $(document).height() && st > $(window).height()) {
-//                 //$('header').addClass("header-shadow "); //done to avoid transition conflict
-//                 $('header').removeClass("slideOutUp").addClass('header-shadow slideInDown ');
-//             }
-//         }
-
-//         if(st == 0 && !$('header').hasClass('slideInDown')){
-//             $('header').removeClass("slideOutUp").addClass('header-shadow-off slideInDown');
-//         }
-        
-//         lastScrollTop = st;
-//     }
-
-// }(window.jQuery, window, document));
-
-
+function about(){
+	 $("html").show("slide", { direction: "left" }, 1000);
+	 //document.location.href = "default.html";
+}
