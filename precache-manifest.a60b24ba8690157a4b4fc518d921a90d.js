c593aadc212ea797ff163042a79bd288f8865b7e
Michael Fernandes <fernb0t@Kaia-Lynnes-MBP.localdomain>
2019-01-24 13:01:41 -0800

    Updates

---
+++ precache-manifest.a60b24ba8690157a4b4fc518d921a90d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9625e84f7407542edbb2",
+    "url": "/Opioid-Crisis/static/css/main.2a8b2952.chunk.css"
+  },
+  {
+    "revision": "9625e84f7407542edbb2",
+    "url": "/Opioid-Crisis/static/js/main.9625e84f.chunk.js"
+  },
+  {
+    "revision": "c8de668e4ed46b2b076c",
+    "url": "/Opioid-Crisis/static/js/1.c8de668e.chunk.js"
+  },
+  {
+    "revision": "7f55edb735964182904a",
+    "url": "/Opioid-Crisis/static/js/runtime~main.7f55edb7.js"
+  },
+  {
+    "revision": "7173f73dafaade1f48f6d645c36dd2d5",
+    "url": "/Opioid-Crisis/static/media/logo.7173f73d.svg"
+  },
+  {
+    "revision": "ffad909ea4f1d5b7de4665c7705966c1",
+    "url": "/Opioid-Crisis/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f62f32efc676675cd4e153949fb6bb2c.js"
+  "/Opioid-Crisis/precache-manifest.a60b24ba8690157a4b4fc518d921a90d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Opioid-Crisis/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
