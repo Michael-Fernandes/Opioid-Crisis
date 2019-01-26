329e011ad5c5f20140d84fa3a257c253dd69b711
Michael Fernandes <m.fern93@gmail.com>
2019-01-25 23:46:27 -0800

    Update Header.js
    Spelling

--- src/Components/Page/Header.js
+++ src/Components/Page/Header.js
@@ -23,7 +23,7 @@ export default class Header extends Component {
     render() {
         return (
             <div className="Header">
-                <div className="headerWrapper"><h1>Opoid Crisis</h1><h2>Global Burden of Disease</h2> </div>  
+                <div className="headerWrapper"><h1>Opioid Crisis</h1><h2>Global Burden of Disease</h2> </div>  
 
                 <button onClick={this.share} className="shareBttn">Share View</button>
 
