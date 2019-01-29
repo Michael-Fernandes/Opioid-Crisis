7eaed614bf29cce8ed14c1048fc74d8466be5abf
Michael Fernandes <fernb0t@Kaia-Lynnes-MBP.localdomain>
2019-01-29 15:29:59 -0800

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import Main from "./Main"
-
+//To see if people use share feature
+import ReactGA from 'react-ga';
 //Need router to handle url sharing
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
+ReactGA.initialize('UA-133428628-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 class App extends Component {
   render() {
